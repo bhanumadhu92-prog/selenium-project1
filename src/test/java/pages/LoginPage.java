fb0328417a9475afe25e47eb9669d21ef0e34192
bhanuteja <bhanumadhu92@gmail.com>
2026-02-24 08:48:48 -0800

    Initial commit - firstpom

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	
+	WebDriver driver;
+	
+	public LoginPage(WebDriver driver) {
+		
+		this.driver = driver;
+	}
+ By username = By.id("username");
+ By password = By.id("password");
+ By submitbtn = By.id("submit");
+ 
+ public void enterUsername (String user) {
+	
+	 driver.findElement(username).sendKeys(user);
+	 
+ }
+ 
+ public void enterPassword(String pass) {
+	 
+	 driver.findElement(password).sendKeys(pass);
+ }
+ 
+ public void clickSubmit() {
+	 driver.findElement(submitbtn).click();
+ }
+ 
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+	@Test
+	public void loginTests()
+	{
+		
+		LoginPage lp = new LoginPage(driver);
+		
+		lp.enterUsername("student");
+		lp.enterPassword("Password123");
+		lp.clickSubmit();
+	}
+	
+	
+}
